8edee59bc1e8e874b552bda79d67b7e3bf2b0ffd
adam-lafontaine <adam.lafontaine@gmail.com>
2023-06-26 11:46:08 -0400

    print values

--- Part1/06/main_043.cpp
+++ Part1/06/main_043.cpp
@@ -239,27 +239,138 @@ namespace REG
     static int sp() { return (int)SP; }
     static int bp() { return (int)BP; }
     static int si() { return (int)SI; }
-    static int di() { return (int)DI; }    
+    static int di() { return (int)DI; }
 
-    static void ax(int v) { AX = (u16)v; }
-    static void bx(int v) { BX = (u16)v; }
-    static void cx(int v) { CX = (u16)v; }
-    static void dx(int v) { DX = (u16)v; }
 
-    static void ah(int v) { AX = (u16)(((v & LOW_8) << 8) + (AX & LOW_8)); }
-    static void bh(int v) { AX = (u16)(((v & LOW_8) << 8) + (BX & LOW_8)); }
-    static void ch(int v) { AX = (u16)(((v & LOW_8) << 8) + (CX & LOW_8)); }
-    static void dh(int v) { AX = (u16)(((v & LOW_8) << 8) + (DX & LOW_8)); }
+    enum class Name : int
+    {
+        ax,
+        bx,
+        cx,
+        dx,
 
-    static void al(int v) { AX = (u16)((AX & HI_8) + (v & LOW_8)); }
-    static void bl(int v) { BX = (u16)((BX & HI_8) + (v & LOW_8)); }
-    static void cl(int v) { CX = (u16)((CX & HI_8) + (v & LOW_8)); }
-    static void dl(int v) { DX = (u16)((DX & HI_8) + (v & LOW_8)); }
+        ah,
+        bh,
+        ch,
+        dh,
 
-    static void sp(int v) { SP = (u16)v; }
-    static void bp(int v) { BP = (u16)v; }
-    static void si(int v) { SI = (u16)v; }
-    static void di(int v) { DI = (u16)v; }
+        al,
+        bl,
+        cl,
+        dl,
+        
+        sp,
+        bp,
+        si,
+        di,
+
+        none = -1
+    };
+
+
+    static int get(Name r)
+    {
+        using R = REG::Name;
+
+        switch (r)
+        {
+        case R::ax: return ax();
+        case R::bx: return bx();
+        case R::cx: return cx();
+        case R::dx: return dx();
+        
+        case R::ah: return ah();
+        case R::bh: return bh();
+        case R::ch: return ch();
+        case R::dh: return dh();
+
+        case R::al: return al();
+        case R::bl: return bl();
+        case R::cl: return cl();
+        case R::dl: return dl();
+
+        case R::sp: return ax();
+        case R::bp: return ax();
+        case R::si: return ax();
+        case R::di: return ax();
+        }
+
+        return -1;
+    }
+
+
+    static void print_all()
+    {
+        auto const print = [](cstr str, int val){ printf("%s: 0x%04x (%d)\n", str, val, val); };
+
+        print("ax", ax());
+        print("bx", cx());
+        print("cx", cx());
+        print("dx", dx());
+        print("sp", sp());
+        print("bp", bp());
+        print("si", si());
+        print("di", di());
+    }       
+}
+
+
+namespace MOV
+{
+    static void ax(int v) { REG::AX = (u16)v; }
+    static void bx(int v) { REG::BX = (u16)v; }
+    static void cx(int v) { REG::CX = (u16)v; }
+    static void dx(int v) { REG::DX = (u16)v; }
+
+    static void ah(int v) { REG::AX = (u16)(((v & REG::LOW_8) << 8) + (REG::AX & REG::LOW_8)); }
+    static void bh(int v) { REG::AX = (u16)(((v & REG::LOW_8) << 8) + (REG::BX & REG::LOW_8)); }
+    static void ch(int v) { REG::AX = (u16)(((v & REG::LOW_8) << 8) + (REG::CX & REG::LOW_8)); }
+    static void dh(int v) { REG::AX = (u16)(((v & REG::LOW_8) << 8) + (REG::DX & REG::LOW_8)); }
+
+    static void al(int v) { REG::AX = (u16)((REG::AX & REG::HI_8) + (v & REG::LOW_8)); }
+    static void bl(int v) { REG::BX = (u16)((REG::BX & REG::HI_8) + (v & REG::LOW_8)); }
+    static void cl(int v) { REG::CX = (u16)((REG::CX & REG::HI_8) + (v & REG::LOW_8)); }
+    static void dl(int v) { REG::DX = (u16)((REG::DX & REG::HI_8) + (v & REG::LOW_8)); }
+
+    static void sp(int v) { REG::SP = (u16)v; }
+    static void bp(int v) { REG::BP = (u16)v; }
+    static void si(int v) { REG::SI = (u16)v; }
+    static void di(int v) { REG::DI = (u16)v; }
+}
+
+
+namespace OP
+{
+    enum class Name : int
+    {
+        mov,
+        add,
+        sub,
+        cmp,
+
+        none = -1
+    };
+
+
+    Name get_op(ASM::InstStr const& inst)
+    {
+        using Op = OP::Name;
+
+        if (!inst.is_valid)
+        {
+            return Op::none;
+        }
+
+        auto str = inst.op.str;
+
+        if (strcmp(str, "mov"))
+        {
+            return Op::mov;
+        }
+
+        return Op::none;
+    }
+
 }
 
 
@@ -267,5 +378,6 @@ int main()
 {
     ASM::print_file("listing_0043_immediate_movs.asm");
 
-
+    printf("Final registers:\n");
+    REG::print_all();
 }
